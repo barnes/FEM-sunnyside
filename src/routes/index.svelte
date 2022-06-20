35d9ad67fba974d97af66ac7a707ab59e9f32f05
Ryan Barnes <ryan.barnes@atlanticbt.com>
2022-06-20 13:01:34 -0400

    desktop style complete

---
+++ src/routes/index.svelte
@@ -0,0 +1,117 @@
+<div class="container md:mx-auto sm:m-0">
+	<div class="p-5 bg-desktop-image-header bg-cover bg-no-repeat">
+		<div class="flex justify-between">
+			<div class="branding">
+				<h1 class="text-white font-extrabold tracking-wide font-sans text-2xl">sunnyside</h1>
+			</div>
+			<div class="menu">
+				<ul class="flex gap-10 text-white items-center font-sans text-lg">
+					<li>About</li>
+					<li>Services</li>
+					<li>Projects</li>
+					<li class="bg-white py-3 px-6 rounded-3xl text-darkblue uppercase">Contact</li>
+				</ul>
+			</div>
+		</div>
+		<div class="flex items-center gap-24 flex-col pt-24">
+			<div class="text-5xl font-serif uppercase text-white tracking-extrawide font-extrabold">
+				We are creatives
+			</div>
+			<img src="/images/icon-arrow-down.svg" class="mb-96" alt="arrow" />
+		</div>
+	</div>
+	<div class="flex">
+		<div class="px-32 py-44 w-1/2">
+			<h2 class="text-5xl font-extrabold font-serif text-darkblue pb-12">Transform your brand</h2>
+			<p class="text-lg text-verygreyblue font-sans pb-12">
+				We are a full-service creative agency specializing in helping brands grow fast. Engage your
+				cleints through compelling visuals that do most of the marketing for you.
+			</p>
+			<a href="#" class="uppercase text-xl font-serif pb-12">Learn More</a>
+		</div>
+		<div class="w-1/2 p-6 bg-[url('/images/desktop/image-transform.jpg')] bg-cover" />
+	</div>
+	<div class="flex flex-row-reverse ">
+		<div class="px-32 py-44 w-1/2">
+			<h2 class="text-5xl font-extrabold font-serif text-darkblue pb-12">
+				Stand out to the right audience
+			</h2>
+			<p class="text-lg text-verygreyblue font-sans pb-12">
+				Using a collaborative formula of designers, researchers, photographers, videographers, and
+				copywriters, we'll build and extend your brand in digital places.
+			</p>
+			<a href="#" class="uppercase text-xl font-serif pb-12">Learn More</a>
+		</div>
+		<div class="w-1/2 p-6 bg-[url('/images/desktop/image-stand-out.jpg')] bg-cover" />
+	</div>
+	<div class="flex">
+		<div class="px-24 pt-96 w-1/2 bg-[url('/images/desktop/image-graphic-design.jpg')] bg-cover">
+			<h2 class="text-center text-decyan text-4xl font-serif pb-8">Graphic Design</h2>
+			<p class="px-24 text-decyan text-center text-lg font-sans pb-12">
+				Great design makes you memorable. We deliver artwork that underscores your brand message and
+				captures potential clients' attention.
+			</p>
+		</div>
+		<div class="px-24 pt-96 w-1/2 p-6 bg-[url('/images/desktop/image-photography.jpg')] bg-cover">
+			<h2 class="text-center  text-4xl font-serif text-deblue pb-8">Photography</h2>
+			<p class="px-24 text-deblue text-center text-lg font-sans pb-12">
+				Increase your credibility by getting the most stunning, high-quality photos that improve
+				your business image.
+			</p>
+		</div>
+	</div>
+	<div class="py-24 px-48">
+		<h2 class="text-center font-serif uppercase tracking-extrawide text-greyblue text-2xl py-12">
+			Client Testimonials
+		</h2>
+		<div class="flex">
+			<div class="flex flex-col items-center pr-12">
+				<img src="/images/image-emily.jpg" class="rounded-full scale-50 mb-6" />
+				<p class="text-lg text-center text-verygreyblue mb-16">
+					We put our trust in Sunnyside and they delivered, making sure our needs were met and
+					deadlines were always hit.
+				</p>
+				<span class="text-xl font-serif text-deblue">Emily R.</span>
+				<span class="text-md font-sans text-greyblue">Marketing Director</span>
+			</div>
+			<div class="flex flex-col items-center pr-12">
+				<img src="/images/image-thomas.jpg" class="rounded-full scale-50 mb-6" />
+				<p class="text-lg text-center text-verygreyblue mb-16">
+					We put our trust in Sunnyside and they delivered, making sure our needs were met and
+					deadlines were always hit.
+				</p>
+				<span class="text-xl font-serif text-deblue">Thomas S.</span>
+				<span class="text-md font-sans text-greyblue">Chief Operating Officer</span>
+			</div>
+			<div class="flex flex-col items-center">
+				<img src="/images/image-jennie.jpg" class="rounded-full scale-50 mb-6" />
+				<p class="text-lg text-center text-verygreyblue mb-16">
+					We put our trust in Sunnyside and they delivered, making sure our needs were met and
+					deadlines were always hit.
+				</p>
+				<span class="text-xl font-serif text-deblue">Jennie F.</span>
+				<span class="text-md font-sans text-greyblue">Business Owner</span>
+			</div>
+		</div>
+	</div>
+	<div class="flex">
+		<img src="/images/desktop/image-gallery-milkbottles.jpg" class="w-1/4" />
+		<img src="/images/desktop/image-gallery-orange.jpg" class="w-1/4" />
+		<img src="/images/desktop/image-gallery-cone.jpg" class="w-1/4" />
+		<img src="/images/desktop/image-gallery-sugarcubes.jpg" class="w-1/4" />
+	</div>
+	<div class="bg-[#97D8C7] py-12">
+		<h2 class="text-center text-3xl font-extrabold text-decyan pb-8">sunnyside</h2>
+		<ul class="text-lg flex justify-center gap-12 text-decyan pb-20">
+			<li>About</li>
+			<li>Services</li>
+			<li>Projects</li>
+		</ul>
+		<ul class="flex justify-center gap-6">
+			<li><img src="images/icon-facebook.svg" alt="facebook icon" /></li>
+			<li><img src="images/icon-instagram.svg" alt="facebook icon" /></li>
+			<li><img src="images/icon-twitter.svg" alt="facebook icon" /></li>
+			<li><img src="images/icon-pinterest.svg" alt="facebook icon" /></li>
+		</ul>
+	</div>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,33 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		colors: {
+			softred: 'hsl(7,99%,70%)',
+			yellow: 'hsl(51, 100%, 49%)',
+			decyan: 'hsl(167,40%, 24%)',
+			darkblue: 'hsl(198, 62%, 26%)',
+			modcyan: 'hsl(168, 34%, 41%)',
+			deblue: 'hsl(212, 27%, 19%)',
+			verygreyblue: 'hsl(213, 9%, 39%)',
+			greyblue: 'hsl(210, 4%, 67%)',
+			white: 'hsl(0,0%,100%)'
+		},
+		fontFamily: {
+			sans: ['Barlow', 'sans-serif'],
+			serif: ['Fraunces', 'serif']
+		},
+		extend: {
+			backgroundImage: {
+				'desktop-image-header': "url('/images/desktop/image-header.jpg')"
+			},
+			letterSpacing: {
+				extrawide: '0.25em'
+			}
+		}
+	},
+
+	plugins: []
+};
+
+module.exports = config;
