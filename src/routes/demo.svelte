016c39af472e47ff083d943ff17a334a220f50cc
Ryan Barnes <ryan.barnes@atlanticbt.com>
2022-06-27 12:18:02 -0400

    styling very nearly complete

--- src/routes/demo.svelte
+++ src/routes/demo.svelte
@@ -1,14 +1,18 @@
-<div class="container mx-auto">
+<div class="md:w-[1440px] md:mx-auto container">
+	<!-- header-->
 	<div
 		class="bg-[url('/images/mobile/image-header.jpg')] 
     lg:bg-[url('/images/desktop/image-header.jpg')] 
     bg-cover px-6 pt-6 h-[535px] lg:h-[800px] bg-center bg-no-repeat w-full"
 	>
+		<!-- nav -->
 		<div class="flex justify-between">
 			<div class="branding">
-				<h1 class="text-white font-extrabold tracking-wide font-sans text-2xl">sunnyside</h1>
+				<h1 class="text-white font-extrabold tracking-wide font-sans text-2xl md:text-4xl">
+					sunnyside
+				</h1>
 			</div>
-			<div class="menu">
+			<div class="menu hidden lg:block">
 				<ul class="flex flex-col lg:flex-row gap-10 text-white items-center font-sans text-lg">
 					<li>About</li>
 					<li>Services</li>
@@ -17,9 +21,19 @@
 				</ul>
 			</div>
 		</div>
+		<!-- mobile menu -->
+		<div class="md:hidden block menu bg-white mr-3">
+			<ul class="flex flex-col gap-10 py-10 items-center font-sans text-lg text-greyblue">
+				<li>About</li>
+				<li>Services</li>
+				<li>Projects</li>
+				<li class="bg-yellow py-3 px-6 rounded-3xl text-darkblue uppercase">Contact</li>
+			</ul>
+		</div>
+		<!-- headline -->
 		<div class="flex items-center gap-24 flex-col">
 			<div
-				class="text-5xl text-center leading-relaxed font-serif uppercase text-white tracking-extrawide font-extrabold"
+				class="md:text-5xl text-4xl text-center pt-16 leading-relaxed font-serif uppercase text-white tracking-extrawide font-extrabold"
 			>
 				We are creatives
 			</div>
@@ -28,30 +42,40 @@
 	</div>
 	<!-- 4x4 grid -->
 	<div class="grid md:grid-cols-2 grid-cols-1">
-		<div class="p-16 md:h-[600px] h-[300px] flex flex-col justify-center">
-			<h2 class="text-5xl font-extrabold font-serif text-darkblue">Transform your brand</h2>
-			<p class="text-lg text-verygreyblue font-sans pb-12">
+		<div class="p-10 lg:p-16 lg:text-left flex flex-col justify-center text-center">
+			<h2 class="text-3xl lg:text-4xl font-extrabold font-serif text-darkblue pb-6">
+				Transform your brand
+			</h2>
+			<p class="lg:text-lg text-verygreyblue font-sans lg:pb-12 pb-6">
 				We are a full-service creative agency specializing in helping brands grow fast. Engage your
 				cleints through compelling visuals that do most of the marketing for you.
 			</p>
-			<a href="#" class="uppercase text-xl font-serif pb-12">Learn More</a>
+			<a href="#" class="uppercase md:text-xl font-serif">Learn More</a>
 		</div>
-		<div
-			class="image bg-[url('images/mobile/image-transform.jpg')] bg-cover md:h-[600px] h-[300px]"
-		/>
-		<div
-			class="image bg-[url('images/mobile/image-stand-out.jpg')] bg-cover md:h-[600px] h-[300px]"
-		/>
-		<div class="p-16 md:h-[600px] h-[300px] flex flex-col justify-center">
-			<h2 class="text-5xl font-extrabold font-serif text-darkblue">Transform your brand</h2>
-			<p class="text-lg text-verygreyblue font-sans pb-12">
-				We are a full-service creative agency specializing in helping brands grow fast. Engage your
-				cleints through compelling visuals that do most of the marketing for you.
+		<div class="image h-full w-auto order-first md:order-none">
+			<img class="hidden md:block" src="images/desktop/image-transform.jpg" alt="transform" />
+			<img class="block md:hidden" src="images/mobile/image-transform.jpg" alt="transform" />
+		</div>
+		<div class="image h-full">
+			<img class="hidden md:block" src="images/desktop/image-stand-out.jpg" alt="stand-out" />
+			<img
+				class="block md:hidden md:h-full"
+				src="images/mobile/image-stand-out.jpg"
+				alt="stand-out"
+			/>
+		</div>
+		<div class="p-10 lg:p-16 flex flex-col justify-center text-center">
+			<h2 class="text-3xl lg:text-4xl font-extrabold font-serif text-darkblue pb-6">
+				Stand out to the right audience
+			</h2>
+			<p class="lg:text-lg text-verygreyblue font-sans lg:pb-12 pb-6">
+				Using a collaborative formula of designers, researchers, photographers, videographers, and
+				copywriters, we'll build and extend your brand in digital places.
 			</p>
-			<a href="#" class="uppercase text-xl font-serif">Learn More</a>
+			<a href="#" class="uppercase md:text-xl font-serif">Learn More</a>
 		</div>
 		<div
-			class=" p-16 bg-[url('/images/desktop/image-graphic-design.jpg')] bg-cover md:h-[600px] h-[300px] flex flex-col justify-end"
+			class="md:p-16 p-8 pt-[30rem] bg-[url('/images/desktop/image-graphic-design.jpg')] bg-cover bg-center flex flex-col justify-end"
 		>
 			<h2 class="text-center text-decyan text-4xl font-serif ">Graphic Design</h2>
 			<p class="text-decyan text-center text-lg font-sans ">
@@ -60,13 +84,110 @@
 			</p>
 		</div>
 		<div
-			class="pt-80 p-16 bg-[url('/images/desktop/image-photography.jpg')] bg-cover md:h-[600px] h-[300px] flex flex-col justify-end"
+			class="md:p-16 p-8 md:pt-[30rem] pt-[30rem] bg-[url('/images/desktop/image-photography.jpg')] bg-cover bg-center flex flex-col justify-end"
 		>
-			<h2 class="text-center  text-4xl font-serif text-deblue">Photography</h2>
-			<p class=" text-deblue text-center text-lg font-sans">
+			<h2 class="text-center text-decyan text-4xl font-serif ">Photography</h2>
+			<p class="text-decyan text-center text-lg font-sans ">
 				Increase your credibility by getting the most stunning, high-quality photos that improve
 				your business image.
 			</p>
 		</div>
+	</div>
+	<!-- testimonals -->
+	<div class="lg:p-24 p-12">
+		<h2 class="text-center font-serif uppercase tracking-extrawide text-greyblue text-2xl py-12">
+			Client Testimonials
+		</h2>
+		<div class="flex flex-col lg:flex-row md:gap-12 gap-4">
+			<div class="flex flex-col items-center">
+				<img
+					src="/images/image-emily.jpg"
+					alt="testimonal-emily"
+					class="rounded-full scale-50 md:mb-6"
+				/>
+				<p class="text-lg text-center text-verygreyblue md:mb-16 mb-8">
+					We put our trust in Sunnyside and they delivered, making sure our needs were met and
+					deadlines were always hit.
+				</p>
+				<span class="text-xl font-serif text-deblue">Emily R.</span>
+				<span class="text-md font-sans text-greyblue">Marketing Director</span>
+			</div>
+			<div class="flex flex-col items-center">
+				<img
+					src="/images/image-thomas.jpg"
+					alt="testimonal-thomas"
+					class="rounded-full scale-50 md:mb-6"
+				/>
+				<p class="text-lg text-center text-verygreyblue md:mb-16 mb-8">
+					We put our trust in Sunnyside and they delivered, making sure our needs were met and
+					deadlines were always hit.
+				</p>
+				<span class="text-xl font-serif text-deblue">Thomas S.</span>
+				<span class="text-md font-sans text-greyblue">Chief Operating Officer</span>
+			</div>
+			<div class="flex flex-col items-center">
+				<img
+					src="/images/image-jennie.jpg"
+					alt="testimonal-jennie"
+					class="rounded-full scale-50 md:mb-6"
+				/>
+				<p class="text-lg text-center text-verygreyblue md:mb-16 mb-8">
+					We put our trust in Sunnyside and they delivered, making sure our needs were met and
+					deadlines were always hit.
+				</p>
+				<span class="text-xl font-serif text-deblue">Jennie F.</span>
+				<span class="text-md font-sans text-greyblue">Business Owner</span>
+			</div>
+		</div>
+	</div>
+	<!-- gallery -->
+	<div class="grid grid-cols-2">
+		<img
+			class="hidden md:block"
+			src="/images/desktop/image-gallery-milkbottles.jpg"
+			alt="gallery-milkbottles"
+		/>
+		<img
+			class="hidden md:block"
+			src="/images/desktop/image-gallery-orange.jpg"
+			alt="gallery-orange"
+		/>
+		<img class="hidden md:block" src="/images/desktop/image-gallery-cone.jpg" alt="gallery-cone" />
+		<img
+			class="hidden md:block"
+			src="/images/desktop/image-gallery-sugarcubes.jpg"
+			alt="gallery-sugarcubes"
+		/>
+
+		<img
+			class="md:hidden block"
+			src="/images/mobile/image-gallery-milkbottles.jpg"
+			alt="gallery-milkbottles"
+		/>
+		<img
+			class="md:hidden block"
+			src="/images/mobile/image-gallery-orange.jpg"
+			alt="gallery-orange"
+		/>
+		<img class="md:hidden block" src="/images/mobile/image-gallery-cone.jpg" alt="gallery-cone" />
+		<img
+			class="md:hidden block"
+			src="/images/mobile/image-gallery-sugar-cubes.jpg"
+			alt="gallery-sugarcubes"
+		/>
+	</div>
+	<div class="bg-[#97D8C7] py-12">
+		<h2 class="text-center text-3xl font-extrabold text-decyan pb-8">sunnyside</h2>
+		<ul class="text-lg flex justify-center gap-12 text-decyan pb-20">
+			<li>About</li>
+			<li>Services</li>
+			<li>Projects</li>
+		</ul>
+		<ul class="flex justify-center gap-6">
+			<li><img src="images/icon-facebook.svg" alt="facebook icon" /></li>
+			<li><img src="images/icon-instagram.svg" alt="facebook icon" /></li>
+			<li><img src="images/icon-twitter.svg" alt="facebook icon" /></li>
+			<li><img src="images/icon-pinterest.svg" alt="facebook icon" /></li>
+		</ul>
 	</div>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,31 @@
-<div class="lg:container lg:mx-auto container">
+<script>
+	import { fly, fade } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	let mobileMenuToggle = false;
+
+	function toggleMenu() {
+		mobileMenuToggle = !mobileMenuToggle;
+	}
+</script>
+
+<div class="md:w-[1440px] md:mx-auto container">
+	<!-- header-->
 	<div
 		class="bg-[url('/images/mobile/image-header.jpg')] 
     lg:bg-[url('/images/desktop/image-header.jpg')] 
     bg-cover px-6 pt-6 h-[535px] lg:h-[800px] bg-center bg-no-repeat w-full"
 	>
+		<!-- nav -->
 		<div class="flex justify-between">
 			<div class="branding">
-				<h1 class="text-white font-extrabold tracking-wide font-sans text-2xl">sunnyside</h1>
+				<h1 class="text-white font-extrabold tracking-wide font-sans text-2xl md:text-4xl">
+					sunnyside
+				</h1>
 			</div>
-			<div class="menu">
+			<div class="lg:hidden" on:click={toggleMenu}>
+				<img src="images/icon-hamburger.svg" alt="mobile-menu-toggle" />
+			</div>
+			<div class="menu hidden lg:block">
 				<ul class="flex flex-col lg:flex-row gap-10 text-white items-center font-sans text-lg">
 					<li>About</li>
 					<li>Services</li>
@@ -17,69 +34,111 @@
 				</ul>
 			</div>
 		</div>
-		<div class="flex items-center gap-24 flex-col">
+		{#if mobileMenuToggle}
+			<!-- mobile menu -->
 			<div
-				class="text-5xl text-center leading-relaxed font-serif uppercase text-white tracking-extrawide font-extrabold"
+				class="flex flex-col"
+				transition:fly={{
+					delay: 250,
+					duration: 300,
+					x: 400,
+					y: 0,
+					opacity: 0.5,
+					easing: quintOut
+				}}
 			>
-				We are creatives
+				<div class="md:hidden self-end rotate-180">
+					<svg height="16" width="16">
+						<polygon points="16,0 0,16 0,0" style="fill:white;" />
+					</svg>
+				</div>
+				<div class="md:hidden block menu bg-white">
+					<ul class="flex flex-col gap-10 py-10 items-center font-sans text-lg text-greyblue">
+						<li>About</li>
+						<li>Services</li>
+						<li>Projects</li>
+						<li class="bg-yellow py-3 px-6 rounded-3xl text-darkblue uppercase">Contact</li>
+					</ul>
+				</div>
 			</div>
-			<img src="/images/icon-arrow-down.svg" alt="arrow" />
-		</div>
+		{:else}
+			<!-- headline -->
+			<div class="flex items-center gap-24 flex-col">
+				<div
+					class="md:text-5xl text-4xl text-center pt-16 leading-relaxed font-serif uppercase text-white tracking-extrawide font-extrabold"
+				>
+					We are creatives
+				</div>
+				<img src="/images/icon-arrow-down.svg" alt="arrow" />
+			</div>
+		{/if}
 	</div>
-	<div class="flex flex-col lg:flex-row items-center lg:h-[600px]">
-		<div class="px-32 py-44 lg:w-1/2">
-			<h2 class="text-5xl font-extrabold font-serif text-darkblue pb-12">Transform your brand</h2>
-			<p class="text-lg text-verygreyblue font-sans pb-12">
+	<!-- 4x4 grid -->
+	<div class="grid md:grid-cols-2 grid-cols-1">
+		<div class="p-10 lg:p-16 lg:text-left flex flex-col justify-center text-center">
+			<h2 class="text-3xl lg:text-4xl font-extrabold font-serif text-darkblue pb-6">
+				Transform your brand
+			</h2>
+			<p class="lg:text-lg text-verygreyblue font-sans lg:pb-12 pb-6">
 				We are a full-service creative agency specializing in helping brands grow fast. Engage your
 				cleints through compelling visuals that do most of the marketing for you.
 			</p>
-			<a href="#" class="uppercase text-xl font-serif pb-12">Learn More</a>
+			<a href="#" class="uppercase md:text-xl font-serif">Learn More</a>
 		</div>
-		<div class="lg:w-1/2">
-			<img src="/images/desktop/image-transform.jpg" class="h-full" />
+		<div class="image h-full w-auto order-first md:order-none">
+			<img class="hidden md:block" src="images/desktop/image-transform.jpg" alt="transform" />
+			<img class="block md:hidden" src="images/mobile/image-transform.jpg" alt="transform" />
 		</div>
-	</div>
-	<div class="flex lg:flex-row-reverse flex-col items-center">
-		<div class="px-32 py-44 lg:w-1/2">
-			<h2 class="text-5xl font-extrabold font-serif text-darkblue pb-12">
+		<div class="image h-full">
+			<img class="hidden md:block" src="images/desktop/image-stand-out.jpg" alt="stand-out" />
+			<img
+				class="block md:hidden md:h-full"
+				src="images/mobile/image-stand-out.jpg"
+				alt="stand-out"
+			/>
+		</div>
+		<div class="p-10 lg:p-16 flex flex-col justify-center text-center">
+			<h2 class="text-3xl lg:text-4xl font-extrabold font-serif text-darkblue pb-6">
 				Stand out to the right audience
 			</h2>
-			<p class="text-lg text-verygreyblue font-sans pb-12">
+			<p class="lg:text-lg text-verygreyblue font-sans lg:pb-12 pb-6">
 				Using a collaborative formula of designers, researchers, photographers, videographers, and
 				copywriters, we'll build and extend your brand in digital places.
 			</p>
-			<a href="#" class="uppercase text-xl font-serif pb-12">Learn More</a>
+			<a href="#" class="uppercase md:text-xl font-serif">Learn More</a>
 		</div>
-		<div class="lg:w-1/2">
-			<img src="images/desktop/image-stand-out.jpg" class="w-full" />
-		</div>
-	</div>
-	<div class="flex flex-col lg:flex-row items-center">
-		<div class="px-24 pt-96 lg:w-1/2 bg-[url('/images/desktop/image-graphic-design.jpg')] bg-cover">
-			<h2 class="text-center text-decyan text-4xl font-serif pb-8">Graphic Design</h2>
-			<p class="px-24 text-decyan text-center text-lg font-sans pb-12">
+		<div
+			class="md:p-16 p-8 pt-[30rem] bg-[url('/images/desktop/image-graphic-design.jpg')] bg-cover bg-center flex flex-col justify-end"
+		>
+			<h2 class="text-center text-decyan text-4xl font-serif ">Graphic Design</h2>
+			<p class="text-decyan text-center text-lg font-sans ">
 				Great design makes you memorable. We deliver artwork that underscores your brand message and
 				captures potential clients' attention.
 			</p>
 		</div>
 		<div
-			class="px-24 pt-96 lg:w-1/2 p-6 bg-[url('/images/desktop/image-photography.jpg')] bg-cover"
+			class="md:p-16 p-8 md:pt-[30rem] pt-[30rem] bg-[url('/images/desktop/image-photography.jpg')] bg-cover bg-center flex flex-col justify-end"
 		>
-			<h2 class="text-center  text-4xl font-serif text-deblue pb-8">Photography</h2>
-			<p class="px-24 text-deblue text-center text-lg font-sans pb-12">
+			<h2 class="text-center text-decyan text-4xl font-serif ">Photography</h2>
+			<p class="text-decyan text-center text-lg font-sans ">
 				Increase your credibility by getting the most stunning, high-quality photos that improve
 				your business image.
 			</p>
 		</div>
 	</div>
-	<div class="py-24">
+	<!-- testimonals -->
+	<div class="lg:p-24 p-12">
 		<h2 class="text-center font-serif uppercase tracking-extrawide text-greyblue text-2xl py-12">
 			Client Testimonials
 		</h2>
-		<div class="flex flex-col lg:flex-row gap-12">
+		<div class="flex flex-col lg:flex-row md:gap-12 gap-4">
 			<div class="flex flex-col items-center">
-				<img src="/images/image-emily.jpg" class="rounded-full scale-50 mb-6" />
-				<p class="text-lg text-center text-verygreyblue mb-16">
+				<img
+					src="/images/image-emily.jpg"
+					alt="testimonal-emily"
+					class="rounded-full scale-50 md:mb-6"
+				/>
+				<p class="text-lg text-center text-verygreyblue md:mb-16 mb-8">
 					We put our trust in Sunnyside and they delivered, making sure our needs were met and
 					deadlines were always hit.
 				</p>
@@ -87,8 +146,12 @@
 				<span class="text-md font-sans text-greyblue">Marketing Director</span>
 			</div>
 			<div class="flex flex-col items-center">
-				<img src="/images/image-thomas.jpg" class="rounded-full scale-50 mb-6" />
-				<p class="text-lg text-center text-verygreyblue mb-16">
+				<img
+					src="/images/image-thomas.jpg"
+					alt="testimonal-thomas"
+					class="rounded-full scale-50 md:mb-6"
+				/>
+				<p class="text-lg text-center text-verygreyblue md:mb-16 mb-8">
 					We put our trust in Sunnyside and they delivered, making sure our needs were met and
 					deadlines were always hit.
 				</p>
@@ -96,8 +159,12 @@
 				<span class="text-md font-sans text-greyblue">Chief Operating Officer</span>
 			</div>
 			<div class="flex flex-col items-center">
-				<img src="/images/image-jennie.jpg" class="rounded-full scale-50 mb-6" />
-				<p class="text-lg text-center text-verygreyblue mb-16">
+				<img
+					src="/images/image-jennie.jpg"
+					alt="testimonal-jennie"
+					class="rounded-full scale-50 md:mb-6"
+				/>
+				<p class="text-lg text-center text-verygreyblue md:mb-16 mb-8">
 					We put our trust in Sunnyside and they delivered, making sure our needs were met and
 					deadlines were always hit.
 				</p>
@@ -106,11 +173,41 @@
 			</div>
 		</div>
 	</div>
+	<!-- gallery -->
 	<div class="grid grid-cols-2">
-		<img src="/images/desktop/image-gallery-milkbottles.jpg" />
-		<img src="/images/desktop/image-gallery-orange.jpg" />
-		<img src="/images/desktop/image-gallery-cone.jpg" />
-		<img src="/images/desktop/image-gallery-sugarcubes.jpg" />
+		<img
+			class="hidden md:block"
+			src="/images/desktop/image-gallery-milkbottles.jpg"
+			alt="gallery-milkbottles"
+		/>
+		<img
+			class="hidden md:block"
+			src="/images/desktop/image-gallery-orange.jpg"
+			alt="gallery-orange"
+		/>
+		<img class="hidden md:block" src="/images/desktop/image-gallery-cone.jpg" alt="gallery-cone" />
+		<img
+			class="hidden md:block"
+			src="/images/desktop/image-gallery-sugarcubes.jpg"
+			alt="gallery-sugarcubes"
+		/>
+
+		<img
+			class="md:hidden block"
+			src="/images/mobile/image-gallery-milkbottles.jpg"
+			alt="gallery-milkbottles"
+		/>
+		<img
+			class="md:hidden block"
+			src="/images/mobile/image-gallery-orange.jpg"
+			alt="gallery-orange"
+		/>
+		<img class="md:hidden block" src="/images/mobile/image-gallery-cone.jpg" alt="gallery-cone" />
+		<img
+			class="md:hidden block"
+			src="/images/mobile/image-gallery-sugar-cubes.jpg"
+			alt="gallery-sugarcubes"
+		/>
 	</div>
 	<div class="bg-[#97D8C7] py-12">
 		<h2 class="text-center text-3xl font-extrabold text-decyan pb-8">sunnyside</h2>
